e2bee48ac5bf58abf093271223aa0b11185182dc
swamyhr <swamyrangannanayaka@gmail.com>
2024-09-16 20:12:58 +0530

    service function for register and button component added

--- src/components/Register.js
+++ src/components/Register.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react'
 import registerInputFields from '../utils/RegisterInputFields'
 import InputField from './InputField'
 import { REGISTER_FORM_TITLE } from '../utils/constants';
+import Button from './Button';
+import { registerUser } from '../services/UserService';
 
 const Register = () => {
 
@@ -16,8 +18,37 @@ const Register = () => {
     const onChange = (e) => {
         setUser({
             ...user,
-            [e.target.name]: [e.target.value]
+            [e.target.name]: e.target.value
         })
+    }
+
+    const getFormattedNewUserDetails = () => {
+        console.log(user);
+
+        return {
+            name: user.name,
+            emailId: user.email,
+            phoneNumber: user.contact,
+            userName: user.username,
+            password: user.password
+        }
+    }
+
+    const register = async (e) => {
+        e.preventDefault();
+        console.log("Called ");
+
+        const newUser = getFormattedNewUserDetails();
+        console.log("useUswer ", newUser);
+
+        try {
+            const response = await registerUser({ ...newUser });
+            const data = await response.json();
+            console.log("Data data", data);
+
+        } catch (error) {
+            console.log(error);
+        }
     }
 
     return (
@@ -27,12 +58,13 @@ const Register = () => {
                 registerInputFields.map((field) => {
                     return <InputField
                         key={field.id}
-                        onChang={onChange}
+                        onChange={onChange}
                         value={user[field.label]}
                         {...field}
                     />
                 })
             }
+            <Button button_name={'Register'} type="submit" onSubmit={register} />
         </React.Fragment>
     )
 }
--- src/utils/RegisterInputFields.js
+++ src/utils/RegisterInputFields.js
@@ -3,12 +3,12 @@ const registerInputFields = [
         name: "name",
         label: "Name",
         type: "text",
-        id: "register_user_name",
+        id: "register_name",
         pattern: ""
     },
     {
         name: "email",
-        label: "email",
+        label: "Email",
         type: "email",
         id: "registrer_email",
         pattern: ""
